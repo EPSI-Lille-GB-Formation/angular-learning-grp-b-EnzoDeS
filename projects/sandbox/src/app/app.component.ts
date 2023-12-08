7217d9fe2d5d28aa6ad7fac172eaa47de3c2b7b9
ezonnes <enzo18072003@gmail.com>
2023-12-08 17:21:22 +0100

    V1

---
+++ projects/sandbox/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `<h1>bonjour</h1>`,
+})
+export class AppComponent {
+  title = 'sandbox';
+}
