12b70ea431249aab70860b4bfa18e0fac8b7ba94
ezonnes <enzo18072003@gmail.com>
2024-01-26 07:55:20 +0100

    Boutons sans fonctionnalité

--- projects/bibliotech/src/app/app.component.ts
+++ projects/bibliotech/src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BOOKS } from './mock/mock-book';
+import '@picocss/pico';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: ``,
+  template: `
+    <article>
+      <h1>Voici la BiblioTech</h1>
+      <div *ngFor="let book of Bibliotech">
+        <article>
+          {{ book.title }}
+          <div class="grid">
+            <div>
+              <br />
+              <button>Modifier</button>
+            </div>
+            <div>
+              <br />
+              <button>Détail</button>
+            </div>
+            <div>
+              <br />
+              <button>Supprimer</button>
+            </div>
+          </div>
+        </article>
+      </div>
+    </article>
+  `,
   styles: ``,
 })
 export class AppComponent {
-  title = 'bibliotech';
+  Bibliotech = BOOKS;
 }
