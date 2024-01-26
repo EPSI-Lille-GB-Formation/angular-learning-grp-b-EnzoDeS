a7148d9e59abdb4600872c59c29c7b8c735dbe70
ezonnes <enzo18072003@gmail.com>
2024-01-26 08:34:04 +0100

    modif code book

--- projects/bibliotech/src/app/component/book.component.ts
+++ projects/bibliotech/src/app/component/book.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { BOOKS } from '../mock/mock-book';
+import { CommonModule } from '@angular/common';
+import '@picocss/pico';
 
 @Component({
   selector: 'app-book.component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-  <p>Moi je suis le book</p>
-  <article *ngFor="let book of Bibliotech">
-{{book.title}}
-</artcile>
+    <p>Moi je suis le book</p>
+    <article *ngFor="let book of Book">
+      {{ book.title }}
+    </article>
   `,
   styles: ``,
 })
