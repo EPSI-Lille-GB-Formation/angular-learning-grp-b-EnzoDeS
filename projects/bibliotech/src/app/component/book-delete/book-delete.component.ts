c75e19a960dcab79d9cd0fdb2dd017e082e99153
ezonnes <enzo18072003@gmail.com>
2024-02-08 19:01:19 +0100

    delete bouton fini mais pas fonctionnel

--- projects/bibliotech/src/app/component/book-delete/book-delete.component.ts
+++ projects/bibliotech/src/app/component/book-delete/book-delete.component.ts
@@ -1,42 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../service/book.service';
-import { book } from '../../models/book';
+import { book } from '../../models/book'; // Assuming 'book' is the correct class name
 import { ActivatedRoute, Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-book-delete',
-  standalone: true,
-  imports: [],
-  template:`<div>
-  <h2>Supprimer le livre</h2>
-  <p>Êtes-vous sûr de vouloir supprimer le livre " {{ Book?.title }} " ?</p>
-  <div class="grid">
-    <button (click)="deleteBook()">
-      Supprimer définitivement
-    </button>
-    <button (click)="cancelButton(Book?.id)">Annuler</button>
-  </div>
-</div>`,
-  styleUrl: './book-delete.component.css'
+  template: ` <div>
+    <h2>Supprimer le livre</h2>
+    <p>Êtes-vous sûr de vouloir supprimer le livre " {{ book?.title }} " ?</p>
+    <div class="grid">
+      <button (click)="deleteBook()">Supprimer définitivement</button>
+      <button (click)="cancelButton(book?.id)">Annuler</button>
+    </div>
+  </div>`,
+  styleUrls: ['./book-delete.component.css'],
 })
-export class BookDeleteComponent implements OnInit{
-
-    Book:book|undefined; 
+export class BookDeleteComponent implements OnInit {
+  Book: book | undefined;
 
   constructor(
     private bookService: BookService,
     private router: Router,
-    private route: ActivatedRoute,
-  ){}
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     const id_book = this.route.snapshot.paramMap.get('id_book');
- 
+
     if (id_book) {
       this.bookService.getBookById(+id_book).subscribe(
         (Book: book | undefined) => {
-          this.Book = book;
+          this.Book = Book;
         },
         (error) => {
           console.error(error);
@@ -44,15 +38,14 @@ export class BookDeleteComponent implements OnInit{
       );
     }
   }
+
   deleteBook(): void {
     if (this.Book) {
-      this.bookService.deleteBook(this.Book.id).subscribe(
-        () => {
-          setTimeout(() => {
-            this.router.navigate(['']);
-          }, 1000);
-        }
-      )
+      this.bookService.deleteBook(this.Book.id).subscribe(() => {
+        setTimeout(() => {
+          this.router.navigate(['']);
+        }, 1000);
+      });
     }
   }
 
